dfa3f9e9c3329ea9918c99a901ce9d59725ab98b
ChanYenFen <72129366+ChanYenFen@users.noreply.github.com>
2025-06-19 14:56:19 +0800

    Update drawing_utils.js

--- js/drawing_utils.js
+++ js/drawing_utils.js
@@ -20,15 +20,24 @@ function drawPolyline(path, c, w) {
 }
 
 function drawTSPAnimated(path) {
-  stroke(255, 0, 0, 200);
-  strokeWeight(0.5);
+  stroke(0, 0, 0, 300);
+  strokeWeight(0.9);
   noFill();
 
   beginShape();
-  for (let i = 0; i < drawIndex && i < path.length; i++) {
+  for (let i = 0; i < min(drawIndex, path.length); i++) {
     vertex(path[i].x, path[i].y);
   }
   endShape();
 
-  if (drawIndex < path.length) drawIndex += 0.5; // 控制速度
+  if (drawIndex < path.length) {
+    drawIndex += 0.5; // 控制動畫速度
+  } else {
+    // 完成後繪製完整路徑以避免消失
+    beginShape();
+    for (let p of path) {
+      vertex(p.x, p.y);
+    }
+    endShape();
+  }
 }
